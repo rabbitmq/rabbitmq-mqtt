9c7d04abdfbb8c71e01bbc64f9363797716b0436
Martin Lasak <martin.lasak@fokus.fraunhofer.de>
2015-04-21 11:57:20 +0200

    Adds test to check client cert auth in conjunction with mqtt
    Due to alternate server configuration this test needs to be explicitly
    requested by setting the following command-line parameter RABBITMQ_MQTT_SSLTEST_ONLY
    
    $ cd rabbitmq-mqtt
    $ export RABBITMQ_MQTT_SSLTEST_ONLY=1
    $ make test

--- package.mk
+++ package.mk
@@ -4,13 +4,19 @@ WITH_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/test.sh
 WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/test/ebin/test
 WITH_BROKER_SETUP_SCRIPTS:=$(PACKAGE_DIR)/test/setup-rabbit-test.sh
 
+ifndef RABBITMQ_MQTT_SSLTEST_ONLY
+  WITH_BROKER_TEST_CONFIG_INPUT:=test.config
+else
+  WITH_BROKER_TEST_CONFIG_INPUT:=test-client-cert-ssl-only.config
+endif
+
 define package_rules
 
 $(PACKAGE_DIR)+pre-test::
 	rm -rf $(PACKAGE_DIR)/test/certs
 	mkdir $(PACKAGE_DIR)/test/certs
 	mkdir -p $(PACKAGE_DIR)/test/ebin
-	sed -e "s|%%CERTS_DIR%%|$(abspath $(PACKAGE_DIR))/test/certs|g" < $(PACKAGE_DIR)/test/src/test.config > $(PACKAGE_DIR)/test/ebin/test.config
+	sed -e "s|%%CERTS_DIR%%|$(abspath $(PACKAGE_DIR))/test/certs|g" < $(PACKAGE_DIR)/test/src/$(WITH_BROKER_TEST_CONFIG_INPUT) > $(PACKAGE_DIR)/test/ebin/test.config
 	make -C $(PACKAGE_DIR)/../rabbitmq-test/certs all PASSWORD=bunnychow DIR=$(abspath $(PACKAGE_DIR))/test/certs
 
 $(PACKAGE_DIR)+clean::
